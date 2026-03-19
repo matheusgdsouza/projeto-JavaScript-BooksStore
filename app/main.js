a318805325ad08890e1cab02e95ca244827284cc
Matheus de Souza <matheusgds3011@gmail.com>
2026-03-19 13:47:40 -0300

    Revisando

--- app/main.js
+++ app/main.js
@@ -1,9 +1,32 @@
 let livros = [];
-const endpoint = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+let endPoint = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 
-getBuscarLivros();
-async function getBuscarLivros() {
-    const res = await fetch(endpoint);
+getBuscaLivros();
+async function getBuscaLivros() {
+    const res = await fetch(endPoint);
     livros = await res.json();
     console.table(livros);
+
+    lendoLivros();
+
 }
+
+async function lendoLivros() {
+    let listaDeLivros = document.getElementById('livros');
+    livros.forEach(livro => {
+        listaDeLivros.innerHTML += `
+        <div class="livro">
+            <img class="livro__imagens" src="${livro.imagem}"
+            alt="${livro.alt}" />
+            <h2 class="livro__titulo">
+                ${livro.titulo}
+            </h2>
+            <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco}0</p>
+        <div class="tags">
+            <span class="tag">${livro.categoria}</span>
+        </div>
+        </div>`
+    })
+}
+
