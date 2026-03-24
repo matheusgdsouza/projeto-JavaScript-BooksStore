82230b1e07bbc871aa8d9505df27eafea13590a3
Matheus de Souza <matheusgds3011@gmail.com>
2026-03-23 22:03:46 -0300

    Feat: Finalizando projeto, adcionando soma de livros disponiveis

--- app/filtrarPorCategoria.js
+++ app/filtrarPorCategoria.js
@@ -7,9 +7,17 @@ function filtrarLivros() {
     let livrosFiltrados = valorBtn == 'disponivel' ? listaDeLivros.filter(livro => livro.quantidade > 0) : listaDeLivros.filter(livro => livro.categoria == valorBtn )
     elementoListaDeLivros.innerHTML = '';
     exibeListaDeLivros(livrosFiltrados);
+    if(valorBtn == 'disponivel'){
+        mostraValorTotalLivrosDisponiveis(livrosFiltrados);
+    }
 }
 
-
-
-
+function mostraValorTotalLivrosDisponiveis(listaFiltrada) {
+    let somaPrecoLivrosDisponiveis = listaFiltrada.reduce((acc, livro) => acc + livro.preco, 0).toFixed(2); 
+    valorTotalLivrosDisponivel.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">${somaPrecoLivrosDisponiveis}</span></p>
+        </div>
+        `;
+}
 
--- app/main.js
+++ app/main.js
@@ -1,20 +1,17 @@
 let listaDeLivros = [];
 const endPointDaAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+const valorTotalLivrosDisponivel = document.querySelector('#valor_total_livros_disponiveis');
 
 getBuscaInformacoesDaAPI();
 
 async function getBuscaInformacoesDaAPI() {
     const res = await fetch(endPointDaAPI);
     listaDeLivros = await res.json();
+    valorTotalLivrosDisponivel.innerHTML = '';
 
-    console.table(listaDeLivros);
     aplicaDesconto(listaDeLivros);
     exibeListaDeLivros(listaDeLivros);
 }
-
-
-
-
 
 
 
